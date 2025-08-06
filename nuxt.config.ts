6b8022f79bd86753eaa8ed06f8e228a93253a0da
Rodrigo Elias <rodrigo.elias@cbd.int>
2025-08-06 14:29:17 -0400

    Formatting suggestions
    Corrects formatting inconsistencies in the locale definitions.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,14 +12,14 @@ export default defineNuxtConfig({
   i18n: {       
     locales: [
       { code: 'ar', iso: 'ar-SA', file: 'ar.json', dir: 'rtl' },
-      { code: 'en', iso: 'en-US', file: 'en.json'             },
-      { code: 'es', iso: 'es-ES', file: 'es.json'             },
-      { code: 'fr', iso: 'fr-FR', file: 'fr.json'             },
-      { code: 'ru', iso: 'ru-RU', file: 'ru.json'             },
-      { code: 'zh', iso: 'zh-CN', file: 'zh.json'             },
+      { code: 'en', iso: 'en-US', file: 'en.json' },
+      { code: 'es', iso: 'es-ES', file: 'es.json' },
+      { code: 'fr', iso: 'fr-FR', file: 'fr.json' },
+      { code: 'ru', iso: 'ru-RU', file: 'ru.json' },
+      { code: 'zh', iso: 'zh-CN', file: 'zh.json' },
     ],
     defaultLocale: 'en',
-    detectBrowserLanguage : {
+    detectBrowserLanguage: {
       alwaysRedirect: true,
       fallbackLocale: 'en'
     },
