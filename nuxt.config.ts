4046cd3809e67b45f3e2df52156304a9e55c6999
RomeoSaundersII <romeo.saunders@gmail.com>
2025-09-25 12:21:05 -0400

    Boilerplate for Navigation (Options bar, megamenu, footer) and Layout with Components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: [
-    "@pinia/nuxt",
-    "nuxt-gtag",
-    "@nuxtjs/robots",
-    "@nuxtjs/i18n",
-    "@nuxt/image",
+    '@pinia/nuxt',
+    'nuxt-gtag',
+    '@nuxtjs/robots',
+    '@nuxtjs/i18n',
+    '@nuxt/image',
   ],
   site: { indexable: false },
   i18n: {
