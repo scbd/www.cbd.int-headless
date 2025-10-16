2317ccd4ce578d8a8673aee848446b0506ff3d63
Rodrigo Elias <rodrigo.elias@cbd.int>
2025-10-16 11:32:43 -0400

    Updates meetings API endpoint
    Updates the meetings API endpoint to use the correct URL.
    
    Also adds temporary error handling. A proper error handling will be implemented with the API client fix.
    
    Relates to CIR-135

--- app/composables/api/use-meetings.ts
+++ app/composables/api/use-meetings.ts
@@ -5,14 +5,15 @@ export default function useMeetingsApi() {
   const getMeetings = async (
     options?: MeetingOptions
   ): Promise<MeetingList> => {
-    const { data } = await useFetch('/api/meetings', {
+    const { data, error } = await useFetch('/api/xyz-meetings', {
       params: {
         sort: options?.sort,
         limit: options?.limit,
-        skip: options?.skip,
+        skip: options?.skip
       },
-      onRequestError: handleError
     });
+
+   if (error.value) throw error.value; // TODO: implement handleError whenever api-client is fixed (Stephane).
 
     const response = data.value || { total: 0, rows: [] };
 
