edd1f0622df06fcdb6b2bc0bbd760b76a4326a71
Rodrigo Elias <rodrigo.elias@cbd.int>
2025-10-16 12:59:49 -0400

    Refactors meeting API call
    Updates the meeting API call to use the correct endpoint and implements a unified error handling function.
    
    This change aligns with the ongoing refactoring of the meetings block and improves the robustness of the API interaction.
    
    Relates to CIR-135

--- app/composables/api/use-meetings.ts
+++ app/composables/api/use-meetings.ts
@@ -1,19 +1,25 @@
 import { handleError, internalServerError } from 'api-client/api-error';
+import type { rest } from 'lodash';
 import type { MeetingList, MeetingOptions } from '~~/types/meeting';
+
+// TODO: replace this with a implement handleError whenever api-client is fixed (Stephane).
+function throwOnError({ error, ...rest }) {
+  if (error.value) throw error.value;
+
+  return rest;
+};
 
 export default function useMeetingsApi() {
   const getMeetings = async (
     options?: MeetingOptions
   ): Promise<MeetingList> => {
-    const { data, error } = await useFetch('/api/xyz-meetings', {
+    const { data, error } = await useFetch<MeetingList>('/api/meetings', {
       params: {
         sort: options?.sort,
         limit: options?.limit,
         skip: options?.skip
       },
-    });
-
-   if (error.value) throw error.value; // TODO: implement handleError whenever api-client is fixed (Stephane).
+    }).then(throwOnError);
 
     const response = data.value || { total: 0, rows: [] };
 
