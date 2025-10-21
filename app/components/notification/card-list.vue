cc891aee90269314bcc70651478dae5f31c82202
RomeoSaundersII <romeo.saunders@gmail.com>
2025-10-21 16:16:32 -0400

    Adds notification card list
    Adds a notification card list to the landing page.
    
    This provides a dedicated space for displaying important
    notifications to the user upon arrival on the page.

---
+++ app/components/notification/card-list.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="content-row d-flex flex-column" :class="itemsProps.classes">
+    <div class="row-title">
+      {{ itemsProps.title }}
+    </div>
+    <div class="content-wrapper d-flex">
+      <notification-card
+        v-if="!itemsProps.error"
+        v-for="notification in itemsProps.items"
+        :notification="notification"
+        :key="notification.id"
+      />
+      <status v-else class="error-loader" />
+    </div>
+    <NuxtLink
+      :to="itemsProps.path"
+      class="btn cbd-btn cbd-btn-outline-more-content"
+    >
+      More notifications
+    </NuxtLink>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import type {
+  Notification,
+  NotificationList,
+  NotificationOptions,
+} from '~~/types/notification';
+import useNotificationsApi from '~/composables/api/use-notifications';
+
+const { t, locale } = useI18n();
+
+const { getNotifications } = useNotificationsApi();
+const options: NotificationOptions = { limit: 4 };
+const { total, rows, error } = await getNotifications(options);
+
+const itemsProps = computed(() => {
+  const classes: string[] = [];
+  const styles: Record<string, string | number> = {};
+
+  /**
+   * TODO: handle aliases and pluralization of @items with i18n
+   * eg. t(`${props.type}.plural`);
+   */
+
+  /**
+   * TODO: replace itemProps.path with named route
+   */
+
+  return {
+    items: rows,
+    error: error,
+    classes,
+    styles,
+    title: 'notifications',
+    type: 'notification',
+    path: '/notification',
+  };
+});
+</script>
