b92340220784b22a84f2e1339fb12279dc61c3b3
Rodrigo Elias <rodrigo.elias@cbd.int>
2025-10-29 12:12:36 -0500

    Improves i18n handling by lazy loading translations
    Loads translations only when the component is mounted or when the locale changes.
    This improves performance by avoiding unnecessary loading of all translations upfront.
    Also, it reorganizes locale files into a folder structure for better maintainability.
    
    Relates to CIR-123-Improve-i18n-handling

--- app/app.vue
+++ app/app.vue
@@ -6,12 +6,21 @@
 </template>
 
 <script setup lang="ts">
-const { t } = useI18n();
+import { loadTranslations } from "~/composables/i18n";
+const { locale, t } = useI18n();
 
 useSeoMeta({
   title: () => t("page.title"),
   ogTitle: () => t("page.title"),
   description: () => t("page.description"),
   ogDescription: () => t("page.description"),
+});
+
+onMounted(async () => {
+  await loadTranslations("pages/index");
+});
+
+watch(locale, async () => {
+  await loadTranslations("pages/index");
 });
 </script>
