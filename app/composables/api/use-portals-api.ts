a46041ec3f35f7302d260a3800e683ca16191204
Rodrigo Elias <rodrigo.elias@cbd.int>
2025-11-05 15:19:02 -0500

    Lint fix portals API composable
    Creates a composable function to fetch portals data from the API.
    
    This function encapsulates the API call to retrieve portal information,
    handling potential errors during the process.

--- app/composables/api/use-portals-api.ts
+++ app/composables/api/use-portals-api.ts
@@ -1,25 +1,25 @@
-import type { Portal } from '~~/types/portal';
-import { PORTALS } from '~~/constants/api-paths';
+import type { Portal } from '~~/types/portal'
+import { PORTALS } from '~~/constants/api-paths'
 
 const handleErrorState = ({
   error,
   ...rest
 }: {
-  [key: string]: any;
-  error: any;
+  [key: string]: any
+  error: any
 }) => {
-  if (error.value) throw error.value;
-  return rest;
-};
+  if (error.value) throw error.value
+  return rest
+}
 
-export default function usePortalsApi() {
+export default function usePortalsApi () {
   const getPortals = async (portal: string): Promise<Portal[]> => {
     const { data } = await useFetch<Portal[]>(PORTALS, {
       params: { portal }
-    }).then(handleErrorState);
+    }).then(handleErrorState)
 
-    return data.value;
-  };
+    return data.value
+  }
 
-  return { getPortals };
+  return { getPortals }
 }
