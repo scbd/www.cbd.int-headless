ad4e659e685fc2d2e7f010b2acc6fa5dde535102
Rodrigo Elias <rodrigo.elias@cbd.int>
2025-11-12 16:30:33 -0500

    Refactors i18n implementation for improved efficiency
    Replaces the `loadTranslations` function with a `useTranslations` composable.
    
    This new composable simplifies the process of loading translations
    by using `import.meta.glob` and `useI18n`, ensuring that translations
    are loaded and updated reactively based on the selected locale.
    
    The JSON structure for translations has been updated to remove the
    nested `page` object, which simplifies accessing translations and makes the
    overall structure more streamlined.

--- app/app.vue
+++ app/app.vue
@@ -6,21 +6,13 @@
 </template>
 
 <script setup lang="ts">
-import { loadTranslations } from "~/composables/i18n";
-const { locale, t } = useI18n();
+import { useTranslations } from '~/composables/i18n'
+const pageTranslation = useTranslations('pages/index')
 
 useSeoMeta({
-  title: () => t("page.title"),
-  ogTitle: () => t("page.title"),
-  description: () => t("page.description"),
-  ogDescription: () => t("page.description"),
-});
-
-onMounted(async () => {
-  await loadTranslations("pages/index");
-});
-
-watch(locale, async () => {
-  await loadTranslations("pages/index");
+  title: () => pageTranslation.t('title'),
+  ogTitle: () => pageTranslation.t('title'),
+  description: () => pageTranslation.t('description'),
+  ogDescription: () => pageTranslation.t('description'),
 });
 </script>
