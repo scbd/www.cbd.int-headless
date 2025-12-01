446264cbe75efa2ec02ae645684d6bdcef74ac67
Rodrigo Elias <rodrigo.elias@cbd.int>
2025-12-01 12:48:07 -0500

    Improves i18n handling in app layout
    Migrates from useSeoMeta to useHead for better i18n support,
    including setting the html language attribute and text direction.
    
    Adds a default social card image.

--- app/app.vue
+++ app/app.vue
@@ -7,12 +7,37 @@
 
 <script setup lang="ts">
 import { useTranslations } from '~/composables/i18n'
+
+const { locale, locales } = useI18n()
 const pageTranslation = useTranslations('pages/index')
 
-useSeoMeta({
+useHead({
+  htmlAttrs: {
+    lang: () => locale.value,
+    dir: () =>  locale.value === 'ar' ? 'rtl' : 'ltr'
+  },
   title: () => pageTranslation.t('title'),
-  ogTitle: () => pageTranslation.t('title'),
-  description: () => pageTranslation.t('description'),
-  ogDescription: () => pageTranslation.t('description'),
+  meta: [
+    {
+      name: 'description',
+      content: () => pageTranslation.t('description')
+    },
+    {
+      property: 'og:title',
+      content: () => pageTranslation.t('title')
+    },
+    {
+      property: 'og:description',
+      content: () => pageTranslation.t('description')
+    },
+    {
+      property: 'og:image',
+      content: () => 'https://www.cbd.int/images/cbd-social-card.png'
+    },
+    {
+      property: 'og:twitterCard',
+      content: 'summary_large_image'
+    }
+  ]
 });
 </script>
