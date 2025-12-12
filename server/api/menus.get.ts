0edac041b8649df0a78032adb75725f5b45710fe
Stephane Bilodeau <stephane.bilodeau@cbd.int>
2025-12-12 16:16:13 -0500

    Improves menu retrieval with caching and filtering
    Refactors menu retrieval to use a caching mechanism, significantly reducing Drupal API calls and improving performance.
    
    Adds filtering options for menu depth and branch, allowing for more specific menu retrieval.
    
    Updates the menu API endpoint to accept a menu code instead of a menu name, and introduces query parameters for depth and branch.

--- server/api/menus.get.ts
+++
@@ -1,6 +0,0 @@
-import { getMenu } from '../../services/drupal'
-
-export default defineEventHandler(async (event) => {
-  const { menu } = getQuery(event) as { menu: string }
-  return await getMenu(menu)
-})
