2dd6ad5b76bbc2bf30536d75040fd0d3de8d8e37
RomeoSaundersII <romeo.saunders@gmail.com>
2025-12-16 11:49:32 -0500

    Updates portal image URL generation
    Updates the way portal image URLs are generated to correctly reference the image assets.

--- app/components/portal/card.vue
+++ app/components/portal/card.vue
@@ -1,28 +1,32 @@
 <template>
-    <div class="content-object portal">
-        <NuxtImg :src="portal.imageUrl" class="content-image" loading="lazy" />
-        <div class="title">{{ portal.title }}</div>
-        <div class="read-on-wrapper">
-            <NuxtLink :to="portal.url" class="read-on">{{ t('viewPortal') }}</NuxtLink>
-        </div>
+  <div class="content-object portal">
+    <NuxtImg :src="portal.imageUrl" class="content-image" loading="lazy" />
+    <div class="title">{{ portal.title }}</div>
+    <div class="read-on-wrapper">
+      <NuxtLink :to="portal.url" class="read-on">{{
+        t('viewPortal')
+      }}</NuxtLink>
     </div>
+  </div>
 </template>
 <i18n src="~~/i18n/dist/app/components/portal/card.json"></i18n>
 
 <script lang="ts" setup>
-import type { Menu as Portal } from '~~/types/menu';
-import { IMAGE_FALLBACK } from '~~/constants/image-paths';
+import type { Menu as Portal } from '~~/types/menu'
+import { IMAGE_FALLBACK } from '~~/constants/image-paths'
 
-const { t } = useI18n();
+const { t } = useI18n()
 
 const props = defineProps<{
-    portal: Portal;
-}>();
+  portal: Portal
+}>()
 
 const portal = computed(() => {
-    return {
-        ...props.portal,
-        imageUrl: props.portal.icon ? `/images/${encodeURIComponent(props.portal.icon)}` : IMAGE_FALLBACK
-    };
-});
+  return {
+    ...props.portal,
+    imageUrl: props.portal.icon
+      ? `/images/${encodeURIComponent(props.portal.icon)}`
+      : IMAGE_FALLBACK
+  }
+})
 </script>
