0d504f6718065a8ba44c557d74d12db8dd8bb0d3
Rodrigo Elias <rodrigo.elias@cbd.int>
2026-01-12 11:35:38 -0500

    Adds i18n support to featured item component
    Implements internationalization for the featured item component,
    ensuring text elements are displayed in the user's preferred language.
    This improves accessibility and caters to a global audience.
    
    CIR-217-i18n-issues

--- app/components/hero/content/featured-item.vue
+++ app/components/hero/content/featured-item.vue
@@ -8,20 +8,23 @@
     <NuxtLink :to="article.alias" class="featured-wrapper">
       <div class="featured-information">
         <div class="featured-taxonomy">
-          <div class="featured-source">CBD</div>
+          <div class="featured-source">{{ t('cbd') }}</div>
           <div class="featured-type">{{ article.bundle }}</div>
         </div>
         <div class="featured-description">{{ article.title }}</div>
       </div>
       <div v-if="isPrimary" class="featured-buttons container-field">
-        <button class="btn cbd-btn cbd-btn-primary btn-lg">View Article</button>
+        <button class="btn cbd-btn cbd-btn-primary btn-lg">{{ t('viewArticle') }}</button>
       </div>
     </NuxtLink>
   </div>
 </template>
+<i18n src="~~/i18n/dist/app/components/hero/content/featured-item.json"></i18n>
 
 <script setup lang="ts">
 import type { Article } from '~~/types/content'
+
+const { t } = useI18n();
 
 const props = defineProps<{
   article: Article
