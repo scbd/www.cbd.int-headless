9d9e0e01d11f293b610e037eb0e7e530e7a6b028
Rodrigo Elias <rodrigo.elias@cbd.int>
2026-01-13 10:15:30 -0500

    Refactors date formatting to use composable
    Migrates the date formatting function to a composable to improve reusability and maintainability across components.
    This ensures consistent date formatting throughout the application, improving the user experience and simplifying future updates.
    
    Relates to CIR-217-i18n-issues

--- utils/date.ts
+++ utils/date.ts
@@ -1,13 +1,17 @@
-export function formatDate (date: Date | string, locale?: string): string {
+export const useFormatDate = (): ((date: Date | string, locale?: string) => string) => {
   const { locale: i18nLocale } = useI18n()
 
-  if (date === undefined || date === null || date === '') return ''
-  if (locale === undefined || locale === null || locale === '') locale = i18nLocale.value ?? 'en'
+  const formatDate = (date: Date | string, locale?: string): string => {
+    if (date === undefined || date === null || date === '') return ''
+    if (locale === undefined || locale === null || locale === '') locale = i18nLocale.value ?? 'en'
 
-  const convertedDate = typeof date === 'string' ? new Date(date) : date
-  return Intl.DateTimeFormat(locale, {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
-  }).format(convertedDate)
-};
+    const convertedDate = typeof date === 'string' ? new Date(date) : date
+    return Intl.DateTimeFormat(locale, {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    }).format(convertedDate)
+  }
+
+  return formatDate
+}
