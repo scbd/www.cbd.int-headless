8fee238b6f3d783928f6202e6505ac8a7cfe203a
Mathieu Desjarlais <github@desmat.ca>
2026-01-20 15:34:45 -0500

    main content page: build menus

--- app/pages/[...alias].vue
+++ app/pages/[...alias].vue
@@ -1,14 +1,33 @@
 <template>
-  <main class="cus-main cus-internal-page d-flex flex-column" role="main">
+  <main
+    class="cus-main cus-internal-page d-flex flex-column"
+    role="main"
+  >
     <div class="main-wrapper">
-      <!-- Horizontal Submenu -->
+      <div class="d-flex flex-column gap-2">
+        <navigation-submenu-horizontal
+          v-if="horizontalMenu1"
+          :items="horizontalMenu1"
+          :url="page.alias"
+        />
+        <navigation-submenu-horizontal
+          v-if="horizontalMenu2"
+          :items="horizontalMenu2"
+          :url="page.alias"
+        />
+      </div>
       <div class="container-xxl d-flex">
-        <!-- Vertical Submenu -->
-        <!-- <navigation-submenu-vertical v-if="vertical.path && vertical.menu && vertical.menu[0]" :path="vertical.path" :menu="vertical.menu[0]"
-        /> -->
+        <navigation-submenu-vertical
+          v-if="verticalMenu"
+          :menu="verticalMenu"
+          :url="page.alias"
+        />
         <article class="cus-article container-fluid d-flex flex-column">
           <!-- Breadcrumbs -->
-          <!-- <navigation-breadcrumbs v-if="menus?.breadcrumbs" :path="menus.breadcrumbs" /> -->
+          <navigation-breadcrumbs
+            v-if="breadcrumbMenu"
+            :items="breadcrumbMenu"
+          />
           <section
             v-dompurify-html="page.body"
             class="rendered-content"
@@ -19,7 +38,10 @@
   </main>
 </template>
 
-<script setup lang="ts">
+<script
+  setup
+  lang="ts"
+>
 import type { Menu } from '~~/types/menu'
 import useContentApi from '~~/app/composables/api/use-content-api'
 import useMenuApi from '~/composables/api/use-menu-api'
@@ -29,83 +51,56 @@ const { getPage } = useContentApi()
 const { getMenu } = useMenuApi()
 
 const page = await getPage(route.path)
+// console.log('pages [...alias].vue', { page })
 
 // The logic below should be probably be moved to a utility and middleware
-const submenus = await getMenu('cbd-header').then((menus) => {
-  const array: { title: string; url: string; submenu?: string }[] = []
-  const m = menus.map((menu) => {
-    return { title: menu.title, url: menu.url, submenu: menu.submenu }
-  })
-  const submenuParents = m.filter(
-    (parent) => typeof parent.submenu === 'string'
-  )
-  array.push(...submenuParents)
-
-  return array
+// @ts-ignore
+const menu = await getMenu(page.value.menu, {
+  // @ts-ignore
+  url: page.value.alias,
+  depth: 1
 })
 
-const getSubmenus = async (submenuName: string) => {
-  return $fetch('/api/menus', { query: { menu: submenuName } })
+const buildTree = (item: any) => {
+  // const depth = item.url?.split('/')?.length;
+  // console.log('buildTree', { item, depth })
+
+  if (item.parent) {
+    return buildTree({
+      ...item.parent,
+      children: [...item.siblings, item]
+    })
+  }
+
+  return item
 }
 
-const { data: menus } = await useAsyncData('menus', async (_nuxtApp) => {
-  const breadcrumbs: { title: string; url: string }[] = []
-
-  const fetchedSubmenus = await Promise.all(
-    submenus.map((submenu) => getSubmenus(submenu.submenu!))
-  )
-
-  const menus = fetchedSubmenus.map((menu, menuIndex) => {
-    return menu.map((item) => ({
-      ...item,
-      parents: submenus[menuIndex] ? [submenus[menuIndex]] : []
-    }))
-  })
-
-  const checkItem = async (item: Menu) => {
-    if (item.url == route.fullPath && item.parents !== undefined) {
-      breadcrumbs.push(...item.parents)
-      breadcrumbs.push({ title: item.title, url: item.url })
-    }
-    if (item.children?.length) {
-      const newChildren = item.children.map((child) => {
-        const parents = new Set<{ title: string; url: string }>()
-        if (item.parents !== undefined)
-          item.parents.map((parent) => {
-            parents.add(parent)
-          })
-        parents.add({ title: item.title, url: item.url })
-        return {
-          ...child,
-          parents: [...parents]
-        }
-      })
-      item.children.length = 0
-      item.children.push(...newChildren)
-      item.children.map((child) => {
-        checkItem(child)
-      })
-    }
+const buildPath = (item: any): any => {
+  if (item.parent) {
+    return [
+      ...buildPath(item.parent),
+      { title: item.title, url: item.url }
+    ]
   }
 
-  menus.map((menu) => {
-    menu.map(checkItem)
-  })
+  return [{ title: item.title, url: item.url }]
+}
 
-  return {
-    breadcrumbs,
-    menus
-  }
-})
+const menuTree = computed(() => buildTree(menu.at(0)));
 
-const vertical = computed(() => {
-  return {
-    menu: menus.value?.menus
-      .flat()
-      .filter((menu) => menu.title === menus.value?.breadcrumbs[1]?.title),
-    path: menus.value?.breadcrumbs.slice(1)
+const breadcrumbMenu = computed(() => buildPath(menu.at(0)));
+
+const horizontalMenu1 = computed(() => {
+  if (menuTree.value) {
+    return [menuTree.value, ...(menuTree.value?.siblings || [])].sort((a, b) => a.position - b.position);
   }
-})
+});
+
+const horizontalMenu2 = computed(() => {
+  return menuTree.value?.children?.sort((a: any, b: any) => a.position - b.position);
+});
+
+const verticalMenu = computed(() => menuTree.value);
 
 definePageMeta({
   layout: 'home'
