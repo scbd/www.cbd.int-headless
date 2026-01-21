fdebd6db05b8e800a223e00966655259f779564b
Mathieu Desjarlais <github@desmat.ca>
2026-01-21 13:17:43 -0500

    rm parent/parents from Menu type

--- types/menu.ts
+++ types/menu.ts
@@ -2,7 +2,7 @@ export interface Menu {
   branchId: string
   parentId?: string | null
   title: string
-  url: string // TODO this is in fact optional
+  url?: string
   position: number
   submenu?: string
   component?: string
@@ -10,9 +10,4 @@ export interface Menu {
   childrenCount: number
   children?: Menu[]
   siblings?: Menu[]
-  parent?: Menu
-  parents?: Array<{ // TODO remove this: use parent recursively
-    title: string
-    url: string
-  }>
 }
