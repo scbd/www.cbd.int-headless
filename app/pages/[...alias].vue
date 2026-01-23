4956c533a96c527dd4c0f873ef2ec407dc52306b
Rodrigo Elias <rodrigo.elias@cbd.int>
2026-01-23 10:23:12 -0500

    Adds basic page functionality
    Introduces the ability to display content from a CMS by fetching it via an API endpoint and rendering it on a page. This lays the groundwork for displaying dynamic content.
    
    The page layout is set to 'home'.

---
+++ app/pages/[...alias].vue
@@ -0,0 +1,27 @@
+<template>
+    <main class="cus-main cus-internal-page d-flex flex-column" role="main">
+        <div class="main-wrapper">
+            <div class="container-xxl d-flex">
+                <article class="cus-article container-fluid d-flex flex-column">
+                    <section
+                        v-dompurify-html="page.body"
+                        class="rendered-content"
+                    ></section>
+                </article>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script lang="ts" setup>
+import useContentApi from '~/composables/api/use-content-api'
+
+const route = useRoute()
+const { getPage } = useContentApi()
+
+const page = await getPage(route.path)
+
+definePageMeta({
+  layout: 'home'
+})
+</script>
