7b4f10e3da2dea61b0d33eb81365cf5389ac816a
Rodrigo Elias <rodrigo.elias@cbd.int>
2026-01-29 14:23:12 -0500

    Adds composable for handling error content
    Creates a composable function to manage error page content based on the HTTP status code.
    
    This composable centralizes the logic for retrieving error messages from i18n files, providing a consistent way to display error information across the application.
    
    The composable also provides a method to clear the error and redirect to the home page, improving the user experience when encountering errors.
    
    Relates to CIR-215

---
+++ app/composables/use-error-content.ts
@@ -0,0 +1,50 @@
+import error403 from '~~/i18n/dist/app/pages/error/403.json'
+import error404 from '~~/i18n/dist/app/pages/error/404.json'
+import error500 from '~~/i18n/dist/app/pages/error/500.json'
+import errorDefault from '~~/i18n/dist/app/pages/error/default.json'
+
+const getMessages = (code: number): Record<string, any> => {
+  switch (code) {
+    case 403:
+      return error403
+    case 404:
+      return error404
+    case 500:
+      return error500
+    default:
+      return errorDefault
+  }
+}
+
+export const useErrorContent = (statusCode: number): ErrorContent => {
+  const { t } = useI18n({
+    useScope: 'local',
+    messages: getMessages(statusCode)
+  })
+  const route = useRoute()
+
+  const handleError = async (): Promise<void> => clearError({ redirect: '/' })
+
+  const errorCode = computed(() => t('errorCode'))
+  const errorCodeMessage = computed(() => t('errorCodeMessage'))
+  const errorMessage = computed(() => t('errorMessage', { path: route.path }))
+  const homeButton = computed(() => t('homeButton'))
+
+  return {
+    errorCode,
+    errorCodeMessage,
+    errorMessage,
+    homeButton,
+    handleError,
+    route
+  }
+}
+
+interface ErrorContent {
+  errorCode: ComputedRef<string>
+  errorCodeMessage: ComputedRef<string>
+  errorMessage: ComputedRef<string>
+  homeButton: ComputedRef<string>
+  handleError: () => Promise<void>
+  route: ReturnType<typeof useRoute>
+}
