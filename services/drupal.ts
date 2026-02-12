f32c99148832ba8d89a2762f4b5c41c77d3c944c
Rodrigo Elias <rodrigo.elias@cbd.int>
2026-02-12 16:09:41 -0500

    Improves route existence validation
    Updates route validation to check if route data, uuid, or bundle are null or empty strings to throw a not found error.
    
    This prevents server errors when attempting to access invalid routes.
    
    Related to CIR-252

--- services/drupal.ts
+++ services/drupal.ts
@@ -46,7 +46,7 @@ export async function getRoute (url: string): Promise<DrupalRouterResponse> {
 
 export async function getContent (url: string): Promise<Content | Article> {
   const route = await getRoute(url)
-  if (route === null || route?.entity?.uuid === null) throw notFound('Route not found.')
+  if (route?.entity?.uuid == null || route.entity.uuid === '' || route.entity.bundle == null || route.entity.bundle === '') throw notFound('Route not found.')
 
   const drupalContent = await drupalApi.getContent(route.entity.uuid, route.entity.bundle)
   if (drupalContent === null) throw notFound('Content not found.')
