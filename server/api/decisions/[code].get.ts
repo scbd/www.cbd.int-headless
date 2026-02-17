69a38e5077db4e542d4e05c54bc304e537666567
Rodrigo Elias <rodrigo.elias@cbd.int>
2026-02-17 14:25:29 -0500

    Retrieves decision by code.
    Replaces decision listing endpoint with a specific decision retrieval based on a provided code.
    
    This change allows fetching a single decision based on its unique code, rather than listing all decisions.
    This is in support of the mega menu dynamic content feature.
    
    CIR-247

--- server/api/decisions/[code].get.ts
+++
@@ -1,8 +0,0 @@
-import type { QueryParams } from '~~/types/api/query-params'
-import { listDecisions } from '~~/services/decision'
-import { apiErrorHandler } from '~~/server/utils/api-error-handler'
-
-export default defineEventHandler(async (event) => {
-  const { sort, limit, skip } = getQuery(event) as QueryParams
-  return await listDecisions({ sort, limit, skip }).catch(apiErrorHandler)
-})
