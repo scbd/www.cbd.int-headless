b7fe6bd43d88861d96333a494e55d9a158d2827e
Rodrigo Elias <rodrigo.elias@cbd.int>
2026-02-24 14:42:15 -0500

    Internationalizes notification search list
    Converts all static text within the notification search list component
    to use i18n keys, enabling multi-language support for various labels
    and information display.
    
    Introduces a new 'Recipients' field to display associated recipients
    for each notification in the search results, including updated styling
    to position the new field correctly within the layout.
    
    Relates to CIR-85

--- app/components/notification/search-list.vue
+++ app/components/notification/search-list.vue
@@ -4,7 +4,11 @@
       <!-- Top pagination -->
       <div class="results-info-wrapper">
         <div class="results-info">
-          Showing {{ startItem }}–{{ endItem }} of {{ notifications.total }} notifications
+          {{ t('showingNotifications', { 
+            startItem: startItem,
+            endItem: endItem,
+            total: notifications.total
+          }) }}
         </div>
         <search-pagination
           :current-page="currentPage"
@@ -35,14 +39,20 @@
               <NuxtLink :to="notification.urls?.[0]" class="title">
                 {{ notification.code }} &ndash; {{ toLocaleText(notification.title) }}
               </NuxtLink>
-              <div class="subjects">
-                Subject(s):
+              <div class="subjects" v-if="notification.themes?.length">
+                {{ t('subjects') }}
                 <template v-for="(theme, i) of notification.themes" :key="i">
                   {{ toLocaleText(theme) }}<template v-if="i < notification.themes.length - 1">, </template>
                 </template>
               </div>
+              <div class="recipients" v-if="notification.recipients?.length">
+                {{ t('recipients') }}
+                <template v-for="(recipient, i) of notification.recipients" :key="i">
+                  {{ toLocaleText(recipient) }}<template v-if="i < notification.recipients.length - 1">, </template>
+                </template>
+              </div>
               <div v-if="notification.actionOn" class="action-required">
-                Action required: {{ toFormatDate(notification.actionOn) }}
+                {{ t('actionRequired') }} {{ toFormatDate(notification.actionOn) }}
               </div>
               <div
                 v-if="toLocaleText(notification.fulltext)"
@@ -50,23 +60,23 @@
               >{{ truncate(toLocaleText(notification.fulltext)) }}</div>
               <div class="read-on-wrapper">
                 <NuxtLink :to="notification.urls?.[0]" class="btn cbd-btn cbd-btn-primary btn cbd-btn-more-content read-on">
-                  View notification
+                  {{ t('viewNotification') }}
                 </NuxtLink>
               </div>
             </div>
             <div class="files" v-if="notification.file">
-              <div class="files-title">Files</div>
+              <div class="files-title">{{ t('files') }}</div>
               <div class="files-available">
                 <NuxtLink class="file-badge" target="_blank" :href="notification.file.url">
                   <NuxtImg
                     v-if="notification.file.type?.match(/application\/pdf/)"
                     src="/images/icons/icon-file-pdf.svg"
-                    alt="PDF"
+                    alt="t('pdf')"
                   />
                   <NuxtImg
                     v-else-if="notification.file.type?.match(/\.doc/)"
                     src="/images/icons/icon-file-doc.svg"
-                    alt="DOC"
+                    alt="t('doc')"
                   />
                 </NuxtLink>
               </div>
@@ -84,12 +94,14 @@
     </div>
   </article>
 </template>
+<i18n src="~~/i18n/dist/app/components/notification/search-list.json"></i18n>
 
 <script setup lang="ts">
 import useNotificationsListApi from '~/composables/api/use-notifications'
 import { IMAGE_FALLBACK } from '~~/constants/image-paths'
 import { ITEMS_PER_PAGE } from '~~/constants/search'
 
+const { t } = useI18n()
 const { toLocaleText } = useLString()
 const { toFormatDate } = useFormatDate()
 
