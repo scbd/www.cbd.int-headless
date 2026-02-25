d40acb57e81ece856de54f553a7612ec080f6751
Rodrigo Elias <rodrigo.elias@cbd.int>
2026-02-25 10:56:39 -0500

    Adds meeting search filtering
    Enables dynamic filtering of meetings by incorporating `fieldQueries` into the API endpoint and service layer. This allows users to apply various search criteria such as title, themes, recipients, and year, enhancing the overall search experience. The Solr query now dynamically combines the default schema filter with user-provided field queries using the `andOr` utility.
    
    Relates to CIR-85

--- server/api/meetings/index.get.ts
+++ server/api/meetings/index.get.ts
@@ -3,6 +3,6 @@ import { listMeetings } from '~~/services/meeting'
 import { apiErrorHandler } from '~~/server/utils/api-error-handler'
 
 export default defineEventHandler(async (event) => {
-  const { sort, limit, skip } = getQuery(event) as QueryParams
-  return await listMeetings({ sort, limit, skip }).catch(apiErrorHandler)
+  const { sort, limit, skip, fieldQueries } = getQuery(event) as QueryParams
+  return await listMeetings({ sort, limit, skip, fieldQueries }).catch(apiErrorHandler)
 })
