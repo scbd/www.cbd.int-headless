aab0da22e6d5ded83cfc839096c1e18de2815d6f
Rodrigo Elias <rodrigo.elias@cbd.int>
2026-03-15 10:11:02 -0400

    feature/CIR-271 - Add social media links to mega-menu
    Replaces placeholder links in the navigation mega-menu with official social media URLs. This change also centralizes the social media paths and other application routes into a dedicated constants file for better maintainability.
    
    Relates to CIR-271

--- constants/url-paths.ts
+++ constants/url-paths.ts
@@ -2,3 +2,10 @@ export const MEETINGS = '/meetings'
 export const NOTIFICATIONS = '/notifications'
 export const STATEMENTS = '/statements'
 export const NBSAPS = 'https://ort.cbd.int/'
+export const SOCIAL_MEDIA = {
+    X: 'https://x.com/UNBiodiversity',
+    FACEBOOK: 'https://www.facebook.com/UNBiodiversity',
+    INSTAGRAM: 'https://www.instagram.com/unbiodiversity',
+    YOUTUBE: 'https://www.youtube.com/@chmcbd',
+    LINKEDIN: 'https://www.linkedin.com/company/unbiodiversity'
+}
