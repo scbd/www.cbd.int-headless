8e294cb889f4d08af5233db92f3d8f4d775b7149
Rodrigo Elias <97183931+rqk2k@users.noreply.github.com>
2026-03-15 12:20:22 -0400

    bugfix/CIR-271 - Add social media links to mega-menu (#105)
    * feature/CIR-271 - Add social media links to mega-menu
    
    Replaces placeholder links in the navigation mega-menu with official social media URLs. This change also centralizes the social media paths and other application routes into a dedicated constants file for better maintainability.
    
    Relates to CIR-271
    
    * feature/CIR-271 - Improve security and syntax of social media links
    
    Adds `rel="noopener noreferrer"` to external social media links for improved security. This change also corrects template attribute quoting in the mega-menu and standardizes indentation in the URL constants file.
    
    Relates to CIR-271

--- app/components/navigation/mega-menu/navigation-mega-menu-social-media.vue
+++ app/components/navigation/mega-menu/navigation-mega-menu-social-media.vue
@@ -2,31 +2,31 @@
   <div class="social-media-follow">
     <div class="social-media-title">{{ t('followUs') }}</div>
     <div class="social-media-icons">
-      <NuxtLink to="#" target="_blank"
+      <NuxtLink :to="SOCIAL_MEDIA.YOUTUBE" target="_blank" rel="noopener noreferrer"
         ><NuxtImg
           src="/images/icons/icon-social-yt.svg"
           :alt="t('youtube')"
           class="social-media-icon"
       /></NuxtLink>
-      <NuxtLink to="#" target="_blank"
+      <NuxtLink :to="SOCIAL_MEDIA.FACEBOOK" target="_blank" rel="noopener noreferrer"
         ><NuxtImg
           src="/images/icons/icon-social-fb.svg"
           :alt="t('facebook')"
           class="social-media-icon"
       /></NuxtLink>
-      <NuxtLink to="#" target="_blank"
+      <NuxtLink :to="SOCIAL_MEDIA.X" target="_blank" rel="noopener noreferrer"
         ><NuxtImg
           src="/images/icons/icon-social-x.svg"
           :alt="t('x')"
           class="social-media-icon"
       /></NuxtLink>
-      <NuxtLink to="#" target="_blank"
+      <NuxtLink :to="SOCIAL_MEDIA.LINKEDIN" target="_blank" rel="noopener noreferrer"
         ><NuxtImg
           src="/images/icons/icon-social-li.svg"
           :alt="t('linkedin')"
           class="social-media-icon"
       /></NuxtLink>
-      <NuxtLink to="#" target="_blank"
+      <NuxtLink :to="SOCIAL_MEDIA.INSTAGRAM" target="_blank" rel="noopener noreferrer"
         ><NuxtImg
           src="/images/icons/icon-social-ig.svg"
           :alt="t('instagram')"
@@ -38,5 +38,6 @@
 <i18n src="~~/i18n/dist/app/components/navigation/mega-menu/navigation-mega-menu-social-media.json"></i18n>
 
 <script setup lang="ts">
+import { SOCIAL_MEDIA } from '~~/constants/url-paths';
 const { t } = useI18n();
 </script>
