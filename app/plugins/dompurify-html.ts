3e69edd703af784f29c053f225daf5ac688aafd3
Rodrigo Elias <rodrigo.elias@cbd.int>
2026-04-10 13:51:25 -0400

    feature/iframe-src - Whitelist cbd.int domains in iframe sanitizer
    Updates the DOMPurify plugin configuration to allow iframes from `.cbd.int` domains. Renames the internal whitelist variable and updates logic comments to reflect that permitted iframe sources now extend beyond YouTube.

--- app/plugins/dompurify-html.ts
+++ app/plugins/dompurify-html.ts
@@ -2,10 +2,13 @@ import VueDOMPurifyHTML from 'vue-dompurify-html'
 import DOMPurify from 'isomorphic-dompurify'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const youtubeSrc = [
+  const iframeAllowedSrc = [
+    /** YouTube */
     /^(https?:)?\/\/(www\.)?youtube\.com\/embed\//,
     /^(https?:)?\/\/youtu\.be\//,
-    /^(https?:)?\/\/if-cdn\.com\//
+    /^(https?:)?\/\/if-cdn\.com\//,
+    /** CBD */
+    /^https?:\/\/[^/]+\.cbd\.int\//
   ]
 
   nuxtApp.vueApp.use(
@@ -23,10 +26,10 @@ export default defineNuxtPlugin((nuxtApp) => {
       enableSSRPropsSupport: true,
       hooks: {
         uponSanitizeElement: (currentNode: any) => {
-          // Limit iframes to only allow YouTube embeds
+          // Limit iframes to only allow whitelisted domains
           if (currentNode.nodeName === 'IFRAME') {
             const src = currentNode.getAttribute('src')
-            if (src === null || src === undefined || !youtubeSrc.some(p => p.test(src))) {
+            if (src === null || src === undefined || !iframeAllowedSrc.some(p => p.test(src))) {
               currentNode.parentNode?.removeChild(currentNode)
             }
           }
